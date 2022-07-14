4ca9733fc8795410a7587441470a7eb84047ec9a
DiegoTavelli <diegotavelli@gmail.com>
2022-07-14 16:08:05 -0300

    04-AdvancedPromises

--- 04-AdvancePromises/homework/src/pledge.js
+++ 04-AdvancePromises/homework/src/pledge.js
@@ -4,6 +4,30 @@ Promises Workshop: construye la libreria de ES6 promises, pledge.js
 ----------------------------------------------------------------*/
 // // TU CÓDIGO AQUÍ:
 
+function $Promise(executor) {
+  this._state = 'pending';
+  this._value = null;
+  if (typeof executor !== 'function') throw new TypeError('executor must be a function');
+
+  // executor -> primer argumento con arrow func, segundo argumento binding this
+  executor((data) => this._internalResolve(data), this._internalReject.bind(this));
+}
+
+
+$Promise.prototype._internalResolve = function (data) {
+  if (this._state === 'pending') {
+    this._state = 'fulfilled';
+    this._value = data;
+  }
+}
+$Promise.prototype._internalReject = function (reason) {
+  if (this._state === 'pending') {
+    this._state = 'rejected';
+    this._value = reason;
+  }
+}
+
+
 
 
 module.exports = $Promise;
