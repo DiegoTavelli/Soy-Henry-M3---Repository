fe1e2762565bfcd42232b9585b95bf77e3361a3a
DiegoTavelli <diegotavelli@gmail.com>
2022-07-22 22:03:07 -0300

    AsyncAwait

--- 07-AsyncAwait/Generators/homework/homework.js
+++ 07-AsyncAwait/Generators/homework/homework.js
@@ -1,6 +1,14 @@
-function fizzBuzzGenerator(max) {
+function* fizzBuzzGenerator(max = Infinity) {
   // Tu código acá:
 
+  let num = 1;
+  while (num <= max) {
+    if (num % 3 === 0 && num % 5 === 0) yield "Fizz Buzz"
+    if (num % 3 === 0) yield "Fizz"
+    if (num % 5 === 0) yield "Buzz"
+    else yield num
+    num++;
+  }
 }
 
 module.exports = fizzBuzzGenerator;
